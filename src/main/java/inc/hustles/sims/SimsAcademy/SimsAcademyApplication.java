a0c8aa93bcc25eb702664838639cce309d259663
SimbaMupfu <scmupfururirwa@gmail.com>
2025-12-02 11:05:57 +0200

    Initial commit

---
+++ src/main/java/inc/hustles/sims/SimsAcademy/SimsAcademyApplication.java
@@ -0,0 +1,13 @@
+package inc.hustles.sims.SimsAcademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimsAcademyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimsAcademyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/inc/hustles/sims/SimsAcademy/SimsAcademyApplicationTests.java
@@ -0,0 +1,13 @@
+package inc.hustles.sims.SimsAcademy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimsAcademyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
